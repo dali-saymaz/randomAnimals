b962e1120b8ce91d6d0c3fcda1151839c2074b96
Hsyn Svndk <hsyn.svndk@hotmail.com>
2020-04-27 21:17:16 +0200

    first commit

---
+++ services/deneme.js
@@ -0,0 +1,6 @@
+import Doctor from './doctor.js';
+
+let deneme=new Doctor
+
+
+console.log(deneme.doctor)
---
+++ services/doctor.js
@@ -0,0 +1,10 @@
+
+export default class Doctor{
+    constructor(){
+        this.doctor=["ali","veli","elli"]
+    }
+}
+let deneme=new Doctor
+console.log(deneme.doctor)
+
+
